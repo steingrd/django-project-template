0e3afb8aca7cfddaf29dda0cd6ee03a148604772
Steingrim Dovland <steingrd@ifi.uio.no>
2008-12-17 17:34:13 +0100

    made project distutils installable

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='django-project-template',
+      version='0.1',
+      author='Steingrim Dovland',
+      author_email='steingrd@ifi.uio.no',
+      url='http://prettyprinted.net/code/django-project-template/',
+      scripts=['scripts/django-project-template.py'], 
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'])
