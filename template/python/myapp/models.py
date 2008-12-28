b391e8c4950a38a551749dd8db8cea8add4cf170
Steingrim Dovland <steingrd@ifi.uio.no>
2008-12-28 13:30:45 +0100

    updated example template project with new specifications

--- template/python/myapp/models.py
+++ template/python/myapp/models.py
@@ -1,1 +1,13 @@
 
+from django.db import models
+ 
+class Entry(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+ 
+    def __unicode__(self):
+        return self.title
--- template/python/myapp/views.py
+++ template/python/myapp/views.py
@@ -1,1 +1,6 @@
 
+from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponse
+
+def myapp_index(request):
+    return HttpResponse('Hello, myapp')
